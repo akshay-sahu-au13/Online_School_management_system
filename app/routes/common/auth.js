e8010baae7e1a67d80a32be93e2d8d33e200a1ba
Akshay Sahu <akki5233@gmail.com>
2021-03-17 23:25:56 +0530

    Initial folder structure

--- app/routes/common/auth.js
+++ app/routes/common/auth.js
@@ -6,6 +6,29 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../../credentials/config');
 
+async function findTeacher(email) {
+    try {
+        let teacher = await Teacher.findOne({email}).lean();
+        if(teacher) {
+            return {err:false, found:true, teacher};
+        } else {
+            return {err:true, found:false, error:"Invalid email"};
+        };
+    } catch (error) {
+        return {err:true, error:e};
+    };
+};
 
-
+async function findStudent(email) {
+    try {
+        let student = await Student.findOne({email}).lean();
+        if(student){
+            return {err:false, found:true, student};
+        } else {
+            return {err: true, found:false, error:"Invalid email"};
+        }
+    } catch (error) {
+        return {err:true, error:e};
+    };
+};
 
