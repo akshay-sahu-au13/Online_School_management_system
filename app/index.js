ec1003c7a24d511030275c6c63406a02aa0248f5
Akshay Sahu <akki5233@gmail.com>
2021-03-17 20:23:34 +0530

    Int

--- app/index.js
+++ app/index.js
@@ -3,14 +3,35 @@ const app = express();
 const PORT = process.env.PORT || 8899;
 const path = require('path');
 const fs = require('fs');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const router = require('./routes/index');
 
 
+app.set('views', path.join(__dirname, "views"))
 app.set('view engine', 'ejs');
 
 app.use(express.json({limit: '3mb'}));
 app.use(express.urlencoded({extended: true, limit: '3mb'}));
 
+mongoose.connect('mongodb://localhost/schoolapp', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+    }).then (()=> console.log('MongoDB Connected')).catch (e=> console.log("DB connection error: ",e.message));
+
+if (process.env.NODE_ENV!="production"){
+    app.use(morgan());
+};
+
+app.use(router);
+
+app.use(function(req, res, next){
+    res.status(404).send({code:404, error: "You seem to be lost"});
+});
 
 app.listen(PORT, ()=> {
     console.log(`Server started on http://localhost:${PORT}`);
 });
+
+
+module.exports = app;
