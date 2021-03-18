03f595c83d9e5f61a2a4a73541937520c6ede6c7
Akshay Sahu <akki5233@gmail.com>
2021-03-18 16:22:53 +0530

    Installed cookie-parser and express=-session

--- app/index.js
+++ app/index.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const router = require('./routes/index');
+const cookie = require('cookie-parser');
+const session = require('express-session');
 
 
 app.set('views', path.join(__dirname, "views"))
@@ -14,10 +16,15 @@ app.set('view engine', 'ejs');
 app.use(express.json({limit: '3mb'}));
 app.use(express.urlencoded({extended: true, limit: '3mb'}));
 
+app.use(cookie());
+app.use(session({
+    secret: "My_session_secret"
+}));
+
 mongoose.connect('mongodb://localhost/schoolapp', {
     useUnifiedTopology: true,
     useNewUrlParser: true
-    }).then (()=> console.log('MongoDB Connected')).catch (e=> console.log("DB connection error: ",e.message));
+    }).then (()=> console.log('MongoDB Connected')).catch (e => console.log("DB connection error: ",e.message));
 
 if (process.env.NODE_ENV!="production"){
     app.use(morgan());
