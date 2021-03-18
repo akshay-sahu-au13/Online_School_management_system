942bb94090e242baa36f398c871b32641d1d1883
Akshay Sahu <akki5233@gmail.com>
2021-03-19 01:58:09 +0530

    Signup and login success using cookies

--- app/routes/common/index.js
+++ app/routes/common/index.js
@@ -9,14 +9,24 @@ router.get('/', (req, res)=> {
 });
 
 router.get('/profile', (req, res)=> {
-    console.log("Student Profile: ",req.user)
-    Student.findById({_id:req.user._id}, (err, data)=> {
-        if (err) return res.status(400).send({error: err.message});
-        if (data){
-            res.status(200).send({studentProfile:data});
-        }
-    })
+    console.log("Student/Teacher Profile: ",req.user)
     
-})
+    if (req.user.user_type === 'student'){
+        Student.findById({_id:req.user._id}, (err, data)=> {
+            if (err) return res.status(400).send({error: err.message});
+            if (data){
+                res.status(200).send({Student_Profile:data});
+            };
+        });
+    } else if (req.user.user_type === 'teacher'){
+        Teacher.findById({_id:req.user._id}, (err, data)=> {
+            if (err) return res.status(400).send({error: err.message});
+            if (data){
+                res.status(200).send({Teacher_Profile:data});
+            };
+        });
+    }
+    
+});
 
 module.exports = router;
