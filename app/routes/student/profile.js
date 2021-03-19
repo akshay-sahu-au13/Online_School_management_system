08c670bdb10e34b21be0a96430482967e5577a01
Akshay Sahu <akki5233@gmail.com>
2021-03-19 23:20:37 +0530

    Using multer for image uploads

--- app/routes/student/profile.js
+++ app/routes/student/profile.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+const img_upload = multer({
+    storage: multer.diskStorage({
+        destination: function(req, res, cb){
+            cb(null, './uploads');
+        },
+        filename: function(req, res, cb){
+            cb(null, 'img - '+ Date.now()+ path.extname(file.originalname));
+        }
+    })
+});
+
