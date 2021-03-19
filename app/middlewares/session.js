dfc778dde11dbf4c10a2ec42d490c13676f7a722
Akshay Sahu <akki5233@gmail.com>
2021-03-20 00:26:43 +0530

    created session auth middleware for both teacher and student

--- app/middlewares/session.js
+++ app/middlewares/session.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-module.exports = function(req, res, next) {
-    if (req.session.user && req.session.user){
-        
-    }
-}
+module.exports.teacher = function(req, res, next) {
+    if (req.session.user && req.user.isLoggedin == true) {
+        if (req.session.user.user_type == "teacher"){
+            next();
+        } else {
+            res.status(400).send({msg: "Invalid authorisation code"});
+        };
+    } else {
+        res.status(400).send({msg:"Send authorisation code"});
+    };
+};
+
+module.exports.student = function(req, res, next) {
+    if (req.session.user && req.user.isLoggedin == true) {
+        if (req.session.user.user_type == "student"){
+            next();
+        } else {
+            res.status(400).send({msg: "Invalid authorisation code"});
+        };
+    } else {
+        res.status(400).send({msg:"Send authorisation code"});
+    };
+};
 
 
-
-
-module.exports = router;
+// module.exports = router;
